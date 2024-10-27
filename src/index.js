e50280720c5f88661ebaf52dfab395207184b66c
Julia Kuzmina <veh.gerli@mail.ru>
2024-10-27 03:59:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function reverse (n) {
-     let tempNumber = n.toString().split('').reverse().join('');
+    let tempNumber = n.toString().split('').reverse().join('');
     if (tempNumber[tempNumber.length - 1] == '-')
-        tempNumber = '-'+tempNumber.slice(0,tempNumber.length - 1);
+        tempNumber = tempNumber.slice(0,tempNumber.length - 1);
     return tempNumber;
 }
